94e0a29b8a31a143b4f4e96ec6430eab73b70987
philrough <44094086+philrough@users.noreply.github.com>
2022-06-10 16:06:28 +0100

    Create environment.py

---
+++ environment.py
@@ -0,0 +1,12 @@
+def before_feature(context, feature):
+    if "skip" in feature.tags:
+        feature.skip("Marked with @skip")
+        return
+
+def before_scenario(context, scenario):
+    if "skip" in scenario.effective_tags:
+        scenario.skip("Marked with @skip")
+        return
+        
+def after_feature(context, feature):
+    print("After Feature 2")
